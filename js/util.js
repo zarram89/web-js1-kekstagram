5e937c76272061eedeb1d4e7b584206bb5f97151
Ramil <zarram89@gmail.com>
2025-12-04 13:27:07 +0500

    feat: adds modules

---
+++ js/util.js
@@ -0,0 +1,22 @@
+// Функция для генерации случайного целого числа в диапазоне от min до max (включительно)
+const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(min, max));
+  const upper = Math.floor(Math.max(min, max));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+// Функция для получения случайного элемента из массива
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+// Функция для создания генератора уникальных ID
+const createIdGenerator = () => {
+  let lastGeneratedId = 0;
+
+  return () => {
+    lastGeneratedId += 1;
+    return lastGeneratedId;
+  };
+};
+
+export { getRandomInteger, getRandomArrayElement, createIdGenerator };
